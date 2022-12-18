f448a7629df540c3365ecd5330899b8ee8a72032
James McNeil <jamesmcneil42@hotmail.co.uk>
2022-12-18 13:27:10 +0100

    [Day15 C#] Completed day 15.

--- Day15/Program.cs
+++ Day15/Program.cs
@@ -1,8 +1,10 @@
-var input = File.ReadAllLines("input.txt").Select(str => str.Split(':').Select(segment => (
+﻿var input = File.ReadAllLines("input.txt").Select(str => str.Split(':').Select(segment => (
   x: long.Parse(segment.Substring(segment.IndexOf("x=") + 2, segment.IndexOf(",") - segment.IndexOf("x=") - 2)),
-  y: long.Parse(segment.Substring(segment.IndexOf("y=") + 2))))).Select(set => (coords: set, dist: Math.Abs(set.First().x - set.Last().x) + Math.Abs(set.First().y - set.Last().y)));
+  y: long.Parse(segment.Substring(segment.IndexOf("y=") + 2))))).Select(set => (coords: set, dist: Distance(set.First(), set.Last()))).ToList();
 
 Console.WriteLine($"Part One: {PartOne()}");
+
+const long maxBoundary = 4000000;
 Console.WriteLine($"Part Two: {PartTwo()}");
 
 int PartOne()
@@ -33,25 +35,82 @@ int PartOne()
 
 ulong PartTwo()
 {
-  throw new Exception("Not just completed");
+  var overlapPoints = new HashSet<(long x, long y)>();
+  for (int i = 0; i < input.Count(); ++i)
+  {
+    var cornersI = GetCornerPoints(input[i].coords.First(), input[i].dist);
+    
+    for (int j = i + 1; j < input.Count() - 1; ++j)
+    {
+      if (input[i].dist + input[j].dist < Distance(input[i].coords.First(), input[j].coords.First())) continue;
+      var cornersJ = GetCornerPoints(input[j].coords.First(), input[j].dist);
+      TryAddOverlapPositions(cornersI, cornersJ, overlapPoints);
+    }
+  }
 
-  //const long maxBoundary = 4000000;
-  //for(long x = 0; x <= maxBoundary; ++x)
-  //{
-  //  for (long y=0; y <= maxBoundary; )
-  //  {
-  //    var firstInteraction = input.FirstOrDefault(set => set.dist < Math.Abs(set.coords.First().x - x) + Math.Abs(set.coords.First().y - y));
-  //    if (firstInteraction == default) return (ulong)x * 4000000ul + (ulong)y;
-  //  }
-  //}
+  // Since we're trying to hit exact edges we'll give it some safety margins.
+  foreach (var overlapPoint in overlapPoints)
+  {
+    for (long i = -2; i <= 2; ++i)
+    {
+      for (long j = -2; j <= 2; ++j)
+      {
+        var position = (x: overlapPoint.x + i, y: overlapPoint.y + j);
+        if (position.x is >= 0 and <= maxBoundary
+            && position.y is >= 0 and <= maxBoundary
+            && !input.Any(set => set.dist >= Distance(set.coords.First(), position)))
+          return (ulong)position.x * 4000000ul + (ulong)position.y;
+      }
+    }
+  }
 
-  //positionsInMiddleOfCircles.RemoveWhere(element => element.x < 0 || element.x > maxBoundary || element.y < 0 || element.y > maxBoundary);
-  //foreach(var position in positionsInMiddleOfCircles)
-  //{
-  //  if (!input.Any(set => set.dist >= Math.Abs(set.coords.First().x - position.x) + Math.Abs(set.coords.First().y - position.y)))
-  //    return (ulong)position.x * 4000000ul + (ulong)position.y;
-  //}
-
-  //// Something went really wrong.
-  //return 0;
+  return 0;
 }
+
+void TryAddOverlapPositions((long x, long y)[] cornersOne, (long x, long y)[] cornersTwo,
+  HashSet<(long x, long y)> toAddTo)
+{
+  // Intersect the line segments of each bounding box.
+  // We do this because theoretically the point we're searching for needs to be right next to an intersection.
+  for (int i = 0; i < cornersOne.Length; ++i)
+  {
+    var p1 = cornersOne[i];
+    var p2 = cornersOne[(i + 1) % cornersOne.Length];
+    
+    for (int j = 0; j < cornersTwo.Length; ++j)
+    {
+      // Co-linear
+      if(i == j) continue;
+      
+      var p3 = cornersTwo[j];
+      var p4 = cornersTwo[(j + 1) % cornersTwo.Length];
+
+      var divisor = (double)(p1.x - p2.x) * (p3.y - p4.y) - (p1.y - p2.y) * (p3.x - p4.x);
+      if(divisor == 0) continue;
+      
+      var t = ((p1.x - p3.x) * (p3.y - p4.y) - (p1.y - p3.y) * (p3.x - p4.x)) / divisor;
+      if (t is < 0.0 or > 1.0) continue;
+      
+      var point = (x: p1.x + (long)(t * (p2.x - p1.x)), y: p1.y + (long)(t * (p2.y - p1.y))); 
+      
+      if (point.x is >= 0 and <= maxBoundary && point.y is >= 0 and <= maxBoundary)
+      {
+        toAddTo.Add(point);
+      }
+    }
+  }
+}
+
+long Distance((long x, long y) coordOne, (long x, long y) coordTwo)
+{
+  return Math.Abs(coordOne.x - coordTwo.x) + Math.Abs(coordOne.y - coordTwo.y);
+}
+
+(long x, long y)[] GetCornerPoints((long x, long y) centre, long dist) =>
+  new[]
+  {
+    (centre.x, centre.y + dist),
+    (centre.x + dist, centre.y),
+    (centre.x, centre.y - dist),
+    (centre.x - dist, centre.y)
+  };
