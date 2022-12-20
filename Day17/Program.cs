258a8f5e360b41d4e944b81c0be641f601db0b48
James McNeil <jamesmcneil42@hotmail.co.uk>
2022-12-20 01:02:16 +0100

    [Day17 C#] Part One code including visulisation.

--- Day17/Program.cs
+++ Day17/Program.cs
@@ -1,8 +1,7 @@
-// WIP
 using System.Text;
 
-var inputString = File.ReadAllText("exampleInput.txt").Trim();
-var rockArrangements = new string[][]
+var inputString = File.ReadAllText("input.txt").Trim();
+var rockArrangements = new[]
 {
   new []{"####"},
   new []{".#.", "###", ".#." },
@@ -11,34 +10,41 @@ var rockArrangements = new string[][]
   new []{"##", "##" }
 };
 
-Console.WriteLine($"Part One: {PartOne()}");
+List<string> outputLines = new List<string>(500);
+Console.WriteLine($"Part One: {Run(2022)}");
 
-int PartOne()
+//Part two, will try to find repeated hashes for the top X tower sections dependant on the rock and input indexes. 
+//Presumably it has to eventually loop around when we have so many rocks.
+//Console.WriteLine($"Part Two: {Run(1000000000000)}");
+
+long Run(long rocksToDrop, long? outputRock = null)
 {
   int inputIndex = 0;
   int rockIndex = 0;
   List<string> rows = new List<string>(5000) { "_______" };
-  for (int rocksFallen = 0; rocksFallen < 2022; ++rocksFallen, ++rockIndex)
+  for (long rocksFallen = 0; rocksFallen < rocksToDrop; ++rocksFallen, ++rockIndex)
   {
-  //  rows.Reverse();
-  //  File.AppendAllText("output.txt", "\n\n\n");
-  //  File.AppendAllLines("output.txt", rows);
-  //  rows.Reverse();
-
-    var rockToFall = rockArrangements[rockIndex % rockArrangements.Length];
+    rockIndex %= rockArrangements.Length;
+    var rockToFall = rockArrangements[rockIndex];
 
     //Position is relative to the top left point
     var rockPos = (x: 2, y: rows.Count + rockToFall.Length + 2);
+
     while(true)
     {
-      int index = (inputIndex++ % inputString.Length);
+      int index = inputIndex++;
+      inputIndex %= inputString.Length;
+      
       int xChange = inputString[index] == '<' ? -1 : 1;
+      if (outputRock.HasValue && outputRock.Value == rocksFallen + 1)
+        OutputView(rows, rockToFall, rockPos, inputString[index]);
+
       bool shouldMove = rockPos.x + xChange >= 0 && rockPos.x + xChange + rockToFall[0].Length <= 7;
       for (int row = 0; row < rockToFall.Length && shouldMove; ++row)
       {
         for (int column = 0; column < rockToFall[row].Length && shouldMove; ++column)
         {
-          int xToCheck = xChange == -1 ? rockPos.x - 1 : rockPos.x + rockToFall[0].Length;
+          int xToCheck = rockPos.x + column + xChange;
           shouldMove = rows.Count <= rockPos.y - row || rows[rockPos.y - row][xToCheck] == '.' || rockToFall[row][column] == '.';
         }
       }
@@ -76,18 +82,50 @@ int PartOne()
     }
   }
 
-  // 3039 is too low??
   // Remove the floor
   return rows.Count - 1;
 }
 
-string AddToString(string present, string toAdd, int startPosition)
+string AddToString(string present, string toAdd, int startPosition, char toSet = '#')
 {
   StringBuilder strB = new StringBuilder(present);
   for (int i = 0; i < toAdd.Length; ++i)
   {
     if (toAdd[i] == '#')
-      strB[startPosition + i] = '#';
+      strB[startPosition + i] = toSet;
   }
+  
   return strB.ToString();
+}
+
+void OutputView(List<string> rows, string[] rockFormation, (int, int) rockPosition, char nextOperation)
+{
+  outputLines.Clear();
+  outputLines.Add($"X Move applied after this: {nextOperation}\n\n");
+  
+  for (int i = rockFormation.Length; i > 0 && rows.Count < rockPosition.Item2 - rockFormation.Length + i + 1; --i)
+  {
+    outputLines.Add("|" + AddToString(".......", rockFormation[^i], rockPosition.Item1, '@') + "|");
+  }
+
+  for (int i = rockPosition.Item2 - rockFormation.Length; i >= rows.Count; --i )
+  {
+    outputLines.Add("|.......|");
+  }
+  
+  for (int i = rows.Count - 1; i >= 0 && i > rows.Count - 15; --i)
+  {
+    if (rockPosition.Item2 >= i && rockPosition.Item2 - rockFormation.Length < i)
+    {
+      int jIndex = rockPosition.Item2 - i;
+      outputLines.Add("|" + AddToString(rows[i], rockFormation[jIndex], rockPosition.Item1, '@') + "|");
+    }
+    else
+    {
+      outputLines.Add($"|{rows[i]}|");
+    }
+  }
+  
+  outputLines.Add("\n\n");
+  File.AppendAllLines("outputFile.txt", outputLines);
 }
